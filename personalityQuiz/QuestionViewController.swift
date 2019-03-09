e07a1d80c6b286fe457daec6603fe2690edef3e6
student15 <student15@student15s-macbook-air.local>
2019-03-09 01:46:22 -0600

    not crashing but stack view is not responding to questions

--- personalityQuiz/QuestionViewController.swift
+++ personalityQuiz/QuestionViewController.swift
@@ -11,14 +11,21 @@ import UIKit
 class QuestionViewController: UIViewController {
     
     
+    @IBOutlet weak var singleStackView: UIStackView!
     
+  
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
     
     
     
     var questions: [Question] = [
         Question(text: "Which food do you like the most?",
                  type: .single,
-                 answers: [
+                 answer: [
                     Answer(text: "Steak", type: .dog),
                     Answer(text: "Fish", type: .cat),
                     Answer(text: "Carrots", type: .rabbit),
@@ -27,7 +34,7 @@ class QuestionViewController: UIViewController {
         
         Question(text: "Which activities do you enjoy?",
                  type: .multiple,
-                 answers: [
+                 answer: [
                     Answer(text: "Swimming", type: .turtle),
                     Answer(text: "Sleeping", type: .cat),
                     Answer(text: "Cuddling", type: .rabbit),
@@ -36,7 +43,7 @@ class QuestionViewController: UIViewController {
         
         Question(text: "How much do you enjoy car rides?",
                  type: .ranged,
-                 answers: [
+                 answer: [
                     Answer(text: "I dislike them", type: .cat),
                     Answer(text: "I get a little nervous", type: .rabbit),
                     Answer(text: "I barely notice them", type: .turtle),
@@ -48,13 +55,40 @@ class QuestionViewController: UIViewController {
     var questionIndex = 0
     
     
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
         // Do any additional setup after loading the view.
     }
     
 
+    func updateUI() {
+        
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+            
+        }
+        
+        
+    }
+    
+    
+    
 
 }
